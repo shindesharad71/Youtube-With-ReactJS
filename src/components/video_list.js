5914f715177c62a155b9d72016cff66369f37d7c
Sharad Shinde <shindesharad71@gmail.com>
2017-08-25 20:08:10 +0530

    select video is complete

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,10 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     const VideoItems = props.videos.map((video) => {
-        return <VideoListItem key={video.etag} video={video}/>
+        return <VideoListItem 
+                    key={video.etag}
+                    video={video}
+                    onVideoSelect={props.onVideoSelect}/>
     });
 
     return ( 
--- src/index.js
+++ src/index.js
@@ -11,10 +11,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         };
 
         YTSearch({ key: API_KEY, term: 'Game of Thrones' }, (videos) => {
-            this.setState({ videos: videos });
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -22,8 +28,11 @@ class App extends Component {
         return ( 
         <div>
             <SearchBar/>
-            <VideoDetail video={this.state.videos[0]}/>
-            <VideoList videos={this.state.videos}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList 
+            videos={this.state.videos} 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            />
         </div>
         );
     }
