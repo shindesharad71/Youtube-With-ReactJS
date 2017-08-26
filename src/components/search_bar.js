9ba78ca958e9a1e2191ac285aa8c00c096581473
Sharad Shinde <shindesharad71@gmail.com>
2017-08-26 16:06:44 +0530

    styling for components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,8 +10,8 @@ class SearchBar extends Component {
 
     render() {
         return (
-          <div >
-            <input onChange = { event => this.setState({ term: event.target.value }) }
+          <div>
+            <input className="search-bar" onChange = { event => this.setState({ term: event.target.value }) }
             />
           </div >
 
